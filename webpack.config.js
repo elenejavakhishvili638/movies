eff36ab4ce8614e8032ba2f9cbaf66a1406f407a
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-11-15 15:58:48 +0400

    feat: install tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,55 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports= {
-    mode: "development",
-    entry: {
-        bundle: path.resolve(__dirname, "src/index.js"),
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
     },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].js",
-        clean: true,
-        assetModuleFilename: "[name][ext]"
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "dist"),
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]
-                    }
-                }
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "movie-project",
-            filename: "index.html",
-            template: "src/template.html"
-        })
-    ]
-}
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "movie-project",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
