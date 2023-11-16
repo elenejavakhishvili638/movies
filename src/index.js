9763a8f49ccab895ad1dbcf9d1841b3d0d710f97
Olysar1 <giorgibarbakadz1@gmail.com>
2023-11-16 11:19:52 +0400

    set up basic data flow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import './style.css';
+//main logic
 
-console.log("the server is updating live now");
+import DataService from './services/DataService';
+import './style.css';
+import MainContent from './ui/MainContent';
+
+const topRatedMovies = new DataService("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
+topRatedMovies.fetchData().then(res=>{
+    // console.log(res.results)
+
+    const mainContent = new MainContent(res.results)
+    mainContent.renderMovieContainer()
+})
+
