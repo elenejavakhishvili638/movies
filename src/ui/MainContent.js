2b9d59806aabf805169ed2c155e14e0eca322cf8
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-11-17 15:42:01 +0400

    style: movies

--- src/ui/MainContent.js
+++ src/ui/MainContent.js
@@ -7,20 +7,29 @@ class MainContent {
     this.moviesArr = moviesArr;
   }
 
-    renderMovieContainer() {
-        const movieContainer = document.createElement("div") // <<creates container for the movies
-        movieContainer.classList.add("grid", "grid-cols-5", "gap-12")
+  renderMovieContainer() {
+    const movieContainer = document.createElement("div"); // <<creates container for the movies
+    movieContainer.classList.add(
+      "grid",
+      "sm:grid-cols-2",
+      "gap-6",
+      "md:grid-cols-4",
+      "md:gap-8",
+      "lg:grid-cols-5",
+      "lg:gap-12"
+    );
 
-        this.moviesArr.forEach(mov => { // <<loops over the movies and passes each of them to the Movie class
-            const m = new Movie(mov) 
-            const movieNode = m.renderMovie()
+    this.moviesArr.forEach((mov) => {
+      // <<loops over the movies and passes each of them to the Movie class
+      const m = new Movie(mov);
+      const movieNode = m.renderMovie();
 
-            movieContainer.appendChild(movieNode)
-        });
+      movieContainer.appendChild(movieNode);
+    });
 
-        console.log(movieContainer)
-        return movieContainer
-    }
+    // console.log(movieContainer);
+    return movieContainer;
+  }
 }
 
 export default MainContent;
