3cacd0cb9cf9128858a154f051b6503bebd0b845
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-11-22 18:18:59 +0400

    refactor: Movie

--- src/ui/Movie.ts
+++ src/ui/Movie.ts
@@ -1,14 +1,76 @@
 // receives movie object and returns a DOM node with its information
 
 import DataService from "../services/DataService";
+import MovieService from "../services/MovieService";
 import MainContent from "./MainContent";
 
-class Movie {
-  constructor(movie) {
+interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface SingleMovie {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  };
+  budget: number;
+  genres: Array<{ id: number; name: string }>;
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: Array<{
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }>;
+  production_countries: Array<{ iso_3166_1: string; name: string }>;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: Array<{
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }>;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+class Movie extends MovieService {
+  private movie: Movie | SingleMovie;
+  constructor(movie: Movie | SingleMovie) {
+    super();
     this.movie = movie;
   }
 
-  createDomElement(elementName) {
+  createDomElement(elementName: string) {
     // <<can be abstracted to another class
     return document.createElement(elementName);
   }
@@ -16,24 +78,20 @@ class Movie {
   renderMovie() {
     const movieWrapper = this.createDomElement("div");
     const imagewrapper = movieWrapper.appendChild(this.createDomElement("div"));
-    const movieImg = imagewrapper.appendChild(this.createDomElement("img"));
+    const movieImg = imagewrapper.appendChild(
+      this.createDomElement("img") as HTMLImageElement
+    );
     const movieDescription = imagewrapper.appendChild(
       this.createDomElement("p")
     );
     const movieTitle = movieWrapper.appendChild(this.createDomElement("span"));
     const movieDate = movieWrapper.appendChild(this.createDomElement("span"));
 
-
-    movieTitle.addEventListener("click", () => {
-      const fetcher = new DataService(
-        `https://api.themoviedb.org/3/movie/${this.movie.id}?language=en-US`
-      );
+    movieTitle.addEventListener("click", async () => {
       const mainContent = new MainContent();
-      fetcher
-        .fetchData()
-        .then((movie) => mainContent.renderSingleMovieDetails(movie)).catch(e=>console.error(e.message));
+      const movie = await this.fetchMovieById(this.movie.id);
+      mainContent.renderSingleMovieDetails(movie);
     });
-
 
     movieWrapper.classList.add("flex", "flex-col");
     movieImg.src = `https://image.tmdb.org/t/p/w500${this.movie.poster_path}`;
